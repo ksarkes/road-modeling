ecabb8a84db4de8cd7ecbebef1397fdb82969cf3
Glongo-PC\Glongo <glongooo4@gmail.com>
2017-04-19 00:47:28 +0300

    addition to merge

--- Assets/Scripts/RoadObjects/Constants.cs
+++ Assets/Scripts/RoadObjects/Constants.cs
@@ -5,14 +5,9 @@ public class Constants
 {
     public static int NO_CAR = -1;
     public static int CAR_OBSTACLE = 1;
-<<<<<<< HEAD
-    public static int SPEED_LIMIT = 20;
-    public static int METERS_PER_EDGE = 2000;
     public static int HALF_CAR_SIZE = 100;
     public static int DST_MULT = 4;
-=======
     public static int SPEED_LIMIT = 1;
     public static int METERS_PER_EDGE = 1000;
     public static int TIME_STEPS_PER_FRAME = 200;
->>>>>>> 8d5429cb502b99f7680f1dbeee8e93ecc5cee690
 }
